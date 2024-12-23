704a5f10e353ba7c0e170f076e9aeac976dd5b6e
ch1ngachgook <aleksey.lashuk2021@gmail.com>
2024-12-23 14:42:58 +0300

    Update MySkills.jsx

--- src/components/MySkills.jsx
+++ src/components/MySkills.jsx
@@ -24,7 +24,7 @@ const MySkills = () => {
           <h3 className="text-xl md:text-2xl font-semibold mb-2">Languages</h3>
           <ul className="list-disc pl-5">
             <li>Russian — native</li>
-            <li>English — B2</li>
+            <li>English — C1</li>
           </ul>
         </div>
       </div>
